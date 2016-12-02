58ca74d7173138238903adb5360a9dc8c32c6ec3
nils <chieftain.proton@gmail.com>
2016-12-02 23:04:49 +0100

    add overrides to rtd theme

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -129,6 +129,8 @@ html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_style = 'css/_rtd_custom.css'
+
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #html_title = 'Master AI-SE v2016-05'
@@ -148,7 +150,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', '_rtd_overrides']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
