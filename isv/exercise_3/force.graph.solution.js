57d32d6115dd053a19bff7902336b477ead87837
nils <nverheyen@gmail.com>
2017-06-11 08:45:14 +0200

    add solution to exercise 3 part 2 of isv

--- isv/exercise_3/force.graph.solution.js
+++ isv/exercise_3/force.graph.solution.js
@@ -2,26 +2,10 @@ function splitKeywords(meta) {
     return $.map(meta.keywords.split(","), $.trim);
 }
 
-// function keywordsEqual(metaA, metaB) {
-//     var keywordsA = splitKeywords(metaA);
-//     var keywordsB = splitKeywords(metaB);
-//
-//     if (keywordsA.length != keywordsB.length) return false;
-//
-//     $.each(keywordsA, function (i) {
-//         var idx = keywordsB.indexOf(keywordsA[i]);
-//         if (idx !== -1) {
-//             keywordsB.splice(idx, 1);
-//         }
-//     })
-//     return keywordsB.length === 0;
-// }
-
 function createNodes() {
     var nodes = [];
     $.each(bib, function(title, meta) {
-        var keywords = splitKeywords(meta);
-        $.each(keywords, function(i, keyword) {
+        $.each(meta.keywordsArr, function(i, keyword) {
             if (nodes.indexOf(keyword) < 0) {
                 nodes.push(keyword);
             }
@@ -37,10 +21,9 @@ function filterPublications() {
     var publications = [];
     var args = arguments;
     $.each(bib, function(title, meta) {
-        var keywords = splitKeywords(meta);
         var count = 0;
         for (var i = 0; i < args.length; i++) {
-            count += keywords.indexOf(args[i]) >= 0 ? 1 : 0;
+            count += meta.keywordsArr.indexOf(args[i]) >= 0 ? 1 : 0;
         }
         if (count == args.length) {
             publications.push({
@@ -51,50 +34,6 @@ function filterPublications() {
     });
     return publications;
 }
-
-// function createLinks(nodes) {
-//     var links = [];
-//     for (var i = 0; i < nodes.length; i++) {
-//
-//         var nodeA = nodes[i];
-//         var otherNodes = $.merge([], nodes);
-//         otherNodes.splice(nodes.indexOf(nodeA), 1);
-//         for (var j = 0; j < otherNodes.length; j++) {
-//             var nodeB = otherNodes[j];
-//
-//             // filter bibliography for publications that contain both keywords
-//             var publications = filterPublications(nodeA, nodeB);
-//
-//             // if more than one publication is found the keywords may be connected
-//             if (publications.length > 1) {
-//
-//                 for (var k = 0; k < publications.length; k++) {
-//
-//                     var publicationA = publications[k];
-//                     var otherPublications = $.merge([], publications);
-//                     otherPublications.splice(publications.indexOf(publicationA), 1);
-//
-//                     for (var l = 0; l < otherPublications.length; l++) {
-//
-//                         var otherPublication = otherPublications[l];
-//                         if (keywordsEqual(publicationA.meta, otherPublication.meta)) {
-//
-//                             var link = {
-//                                 source: nodeA,
-//                                 target: nodeB
-//                             }
-//                             if (links.indexOf(link) === -1) {
-//                                 links.push(link);
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     return links;
-// }
-
 
 function containsLink(link, links) {
     return links.find(function (_link) {
@@ -132,17 +71,127 @@ function createLinks(nodes) {
     return links;
 }
 
+function addKeywordsArray() {
+    $.each(bib, function(title, meta) {
+        var keywords = splitKeywords(meta);
+        bib[title].keywordsArr = keywords;
+    })
+}
+
+function calculateLinkCount(node, graph) {
+    var links = graph.links.filter(function (link) {
+        return link.source == node || link.target == node;
+    });
+    return links.length;
+}
+
+function showSelectedNodeInfo(keyword, publicationCount) {
+
+    $('#node_info').detach();
+
+    var html =
+        '<section id="node_info">' +
+            '<span class="keyword">' + keyword + '</span>' +
+            '<span class="publication_count">' + publicationCount + '</span>' +
+        '</section>'
+    ;
+    $('#selection_details').append(html);
+}
+
+function createPublicationHtml(publication) {
+    var html =
+        '<section class="publication">' +
+            '<dl>'+
+                '<dt>Title</dt>' +
+                '<dd>' + publication.meta.title + '</dd>' +
+                '<dt>Author</dt>' +
+                '<dd>' + publication.meta.author + '</dd>' +
+                '<dt>Year</dt>' +
+                '<dd>' + publication.meta.year + '</dd>' +
+            '</dl>' +
+        '</section>'
+    ;
+    return html;
+}
+
+function showPublications(publications) {
+
+    $('#publications').detach();
+
+    var html = '<ul id="publications">';
+    publications.forEach(function (publication) {
+        html = html + '<li>' + createPublicationHtml(publication) + '</li>';
+    });
+    html = html + "</ul>";
+    $('#selection_details').append(html);
+}
+
+function handleNodeClick(d, i) {
+    var publications = [];
+    $.each(bib, function(title, meta) {
+        if (meta.keywordsArr.indexOf(d.id) >= 0) {
+            var publication = {
+                title: title,
+                meta: meta
+            };
+            publications.push(publication);
+        }
+    });
+
+    if (selectedNode !== undefined) {
+        selectedNode.attr('class', 'nodes');
+    }
+    selectedNode = d3.select(this);
+    selectedNode.attr('class', 'nodes selected');
+
+    showSelectedNodeInfo(selectedNode.select('title').text(), publications.length);
+    showPublications(publications);
+}
+
+addKeywordsArray();
 var nodes = createNodes();
 var links = createLinks($.map(nodes, function(node) {
     return node.id;
 }));
 
+var graphSettings = {
+    minCircleRadius: 3,
+    maxCircleRadius: 8,
+    getRadius: function(minLinks, maxLinks, links) {
+
+        // factor to get the share how much percent the link count takes from minLinks to maxLinks
+        var linkFactor = 100 / (maxLinks - minLinks);
+
+        // 0 <= share <= 1: how much percentage does the link count take from minLinks to maxLinks
+        var share = (linkFactor * (links - minLinks)) / 100;
+
+        /*
+        max radius minus min radius contains the space between.
+        multiplied with the share is equal to radius needed if min radius was zero.
+        add min radius to get the final radius of the circle
+         */
+        var radius = (this.maxCircleRadius - this.minCircleRadius) * share + this.minCircleRadius;
+        return radius;
+    }
+};
+
 /* Graph */
-// var graph = {nodes: [{id: "a"}, {id: "b"}], links: [{source: "a", target: "b"}]};
 var graph = {
     nodes: nodes,
     links: links
 };
+
+var minLinks = $.map(graph.nodes, function (node) {
+    return calculateLinkCount(node.id, graph);
+}).reduce(function (a, b) {
+    return Math.min(a, b);
+});
+
+var maxLinks = $.map(graph.nodes, function (node) {
+    return calculateLinkCount(node.id, graph);
+}).reduce(function (a, b) {
+    return Math.max(a, b);
+});
 
 /* Force-directed graph layout based on D3js example (see sources)*/
 var linkColor = d3.scaleLinear()
@@ -167,16 +216,21 @@ var link = svg.append("g")
     .enter().append("line")
     .attr("stroke", "#aaa");
 
+var selectedNode;
 var node = svg.append("g")
     .attr("class", "nodes")
     .selectAll("circle")
     .data(graph.nodes)
-    .enter().append("circle")
-    .attr("r", 5)
+    .enter()
+    .append("circle")
+    .attr("r", function (d) {
+        return graphSettings.getRadius(minLinks, maxLinks, calculateLinkCount(d.id, graph));
+    })
     .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
-        .on("end", dragended));
+        .on("end", dragended))
+    .on('click', handleNodeClick);
 
 node.append("title")
     .text(function (d) {
